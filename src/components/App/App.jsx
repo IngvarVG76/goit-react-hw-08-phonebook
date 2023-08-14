a02928254baac5e509e9e09505ad66ae6f35442e
Igor Grybanov <igor.gribanov@gmail.com>
2023-08-14 21:39:15 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // if (!selectToken || !selectAuthentificated) return;
+    // if (!selectToken || selectAuthentificated) return;
     if (!selectAuthentificated) return;
 
     dispatch(refreshUserThunk());
